fa3078d73962e82d2b109bbc1e9a3805d760ad25
macbookair <acmkantemir@gmail.com>
2023-10-17 00:42:01 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/buguzei/8e-bot/internal"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load("config/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	internal.Start(bot)
+}
---
+++ internal/bot.go
@@ -0,0 +1,110 @@
+package internal
+
+import (
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+)
+
+func Start(bot *tgbotapi.BotAPI) {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	var forbiddenWords []string
+
+	wordStage := make(map[int64]bool)
+
+	msgID := make(map[int64]int)
+
+	for update := range updates {
+		if update.CallbackQuery != nil {
+			chatID := update.CallbackQuery.From.ID
+
+			switch update.CallbackData() {
+			case "cancel":
+				delMsg := tgbotapi.NewDeleteMessage(chatID, msgID[chatID])
+
+				_, err := bot.Send(delMsg)
+				if err != nil {
+					log.Println(err)
+				}
+
+				wordStage[chatID] = false
+			}
+			continue
+		}
+
+		if update.Message != nil {
+			if update.Message.Chat.IsPrivate() {
+				chatID := update.Message.Chat.ID
+
+				if wordStage[chatID] {
+					forbiddenWords = append(forbiddenWords, update.Message.Text)
+					fmt.Println(forbiddenWords)
+				}
+
+				switch update.Message.Text {
+				case "/newword":
+					msg := tgbotapi.NewMessage(chatID, "Введите новое запрещенное слово:")
+					msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+						tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("Отмена", "cancel")),
+					)
+
+					msgConfig, err := bot.Send(msg)
+					if err != nil {
+						log.Println(err)
+					}
+
+					msgID[chatID] = msgConfig.MessageID
+
+					wordStage[chatID] = true
+				}
+				continue
+			}
+
+			if update.Message.Chat.IsGroup() || update.Message.Chat.IsSuperGroup() {
+				fmt.Println("hwwwwww")
+				chatID := update.Message.Chat.ID
+
+				msgID[chatID] = update.Message.MessageID
+				DeleteForbiddenWord(bot, chatID, msgID[chatID], update.Message.Text, forbiddenWords)
+			}
+		}
+
+	}
+}
+
+func DeleteForbiddenWord(bot *tgbotapi.BotAPI, chatID int64, msgID int, text string, forbiddenWords []string) {
+	for _, word := range forbiddenWords {
+		if len(word) <= len(text) {
+			fmt.Println(222)
+			for i := 0; i < len(text)-len(word)+1; i++ {
+				fmt.Println(i+len(word), len(text))
+				if i+len(word) == len(text) {
+					if text[i:] == word {
+
+						delMsg := tgbotapi.NewDeleteMessage(chatID, msgID)
+
+						_, err := bot.Send(delMsg)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+					continue
+				}
+
+				if text[i:len(word)+i] == word {
+
+					delMsg := tgbotapi.NewDeleteMessage(chatID, msgID)
+
+					_, err := bot.Send(delMsg)
+					if err != nil {
+						log.Println(err)
+					}
+				}
+			}
+		}
+	}
+}
